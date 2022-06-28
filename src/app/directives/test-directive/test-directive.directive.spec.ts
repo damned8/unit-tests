a89b95df9768442700a2f66dc335d3432495491c
forheadhuntera <forheadhuntera@gmail.com>
2022-06-28 07:30:25 +0300

    initial

--- src/app/directives/test-directive/test-directive.directive.spec.ts
+++ src/app/directives/test-directive/test-directive.directive.spec.ts
@@ -1,8 +1,56 @@
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  DebugElement,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { TestDirectiveDirective } from './test-directive.directive';
 
+@Component({
+  template: `<div
+    #ref="appTestDirectiveRef"
+    [appTestDirective]="'foo'"
+    [appTestDirectiveColor]="textColor"
+  >
+    123
+  </div>`,
+})
+class TestWrapperComponent {
+  @ViewChild('ref', { static: true })
+  element!: ElementRef<TestDirectiveDirective>;
+  textColor = 'red';
+}
+
 describe('TestDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TestDirectiveDirective();
-    expect(directive).toBeTruthy();
+  let component: TestWrapperComponent;
+  let fixture: ComponentFixture<TestWrapperComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestWrapperComponent, TestDirectiveDirective],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestWrapperComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should make text colorize', () => {
+    const color = 'blue';
+    component.textColor = color;
+    fixture.detectChanges();
+
+    expect(debugElement.query(By.css('div')).nativeElement.style.color).toBe(
+      color
+    );
   });
 });
--- src/app/directives/test-directive/test-directive.directive.ts
+++ src/app/directives/test-directive/test-directive.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appTestDirective]'
+  selector: '[appTestDirective]',
+  exportAs: 'appTestDirectiveRef',
 })
-export class TestDirectiveDirective {
+export class TestDirectiveDirective implements OnInit {
+  @Input('appTestDirective') inputData?: string;
+  @Input() appTestDirectiveColor?: string;
 
-  constructor() { }
-
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit(): void {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', this.appTestDirectiveColor);
+  }
 }
