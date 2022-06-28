bcfd36ca22596ed8c700a20549be5c38beab0316
forheadhuntera <forheadhuntera@gmail.com>
2022-06-28 06:15:22 +0300

    initial

--- src/app/components/test-compnent/test-compnent.component.spec.ts
+++ src/app/components/test-compnent/test-compnent.component.spec.ts
@@ -1,9 +1,14 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { TestCompnentComponent } from './test-compnent.component';
 
+const TEST_COMPONENT_INPUT_STAB = ['FOO', 'BAR', 'BAZ'];
 describe('TestCompnentComponent', () => {
   let component: TestCompnentComponent;
   let fixture: ComponentFixture<TestCompnentComponent>;
+  let debugElement: DebugElement;
+  let spy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,7 +17,15 @@ describe('TestCompnentComponent', () => {
 
     fixture = TestBed.createComponent(TestCompnentComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
+
+    spy = spyOn(component, 'getEmitValue').and.returnValue(
+      TEST_COMPONENT_INPUT_STAB
+    );
+  });
+
+  afterEach(() => {
   });
 
   it('should create', () => {
@@ -20,7 +33,22 @@ describe('TestCompnentComponent', () => {
   });
 
   it('should have test-compnent works!', () => {
-    const compiled = fixture.debugElement.nativeElement as HTMLElement;
-    expect(compiled.querySelector('p')?.innerText).toBe('test-compnent works!');
+    expect(debugElement.query(By.css('p')).nativeElement.innerText).toContain(
+      'test-compnent works!'
+    );
+  });
+
+  it('should have input and display it', () => {
+    component.input = TEST_COMPONENT_INPUT_STAB;
+    fixture.detectChanges();
+    const elements = debugElement.queryAll(By.css('span')).length;
+    expect(elements).toEqual(TEST_COMPONENT_INPUT_STAB.length);
+  });
+
+  it('should have output', () => {
+    component.output.subscribe((v) =>
+      expect(v).toEqual(TEST_COMPONENT_INPUT_STAB)
+    );
+    debugElement.query(By.css('button')).nativeElement.click();
   });
 });
--- src/app/components/test-compnent/test-compnent.component.ts
+++ src/app/components/test-compnent/test-compnent.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test-compnent',
   templateUrl: './test-compnent.component.html',
-  styleUrls: ['./test-compnent.component.scss']
+  styleUrls: ['./test-compnent.component.scss'],
 })
 export class TestCompnentComponent implements OnInit {
+  @Input() input!: string[];
+  @Output() output =  new EventEmitter<string[]>()
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  emit(): void {
+    this.output.emit(this.getEmitValue())
   }
 
+  getEmitValue(): string[] {
+    return ['foo', 'bar']
+  }
 }
